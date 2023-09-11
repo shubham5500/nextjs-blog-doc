3104c6656ddc3bb04c02e0ea58238e26241753d3
shubham yadav <shubham.yadav@in.ey.com>
2023-09-11 12:48:28 +0530

    first commit

---
+++ pages/index.js
@@ -0,0 +1,47 @@
+import Head from "next/head";
+import Layout from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+import Link from "next/link";
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home={true}>
+      <Head>
+        <title>Create Next App</title>
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+      </Head>
+
+      <section className={utilStyles.headingMd}>
+        <p className="text-red-400">[Your Self Introduction]</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{" "}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+      <section>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`posts/${id}`}>{title}</Link>
+
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
---
+++ pages/posts/[id].js
@@ -0,0 +1,46 @@
+import React from "react";
+import Layout from "../../components/layout";
+import { getAllPostIds, getPostData, sayHello } from "../../lib/posts";
+import Head from "next/head";
+import Date from "../../components/date";
+import utilStyles from '../../styles/utils.module.css';
+
+
+const Post = ({ postData }) => {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
+};
+
+export default Post;
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const id = params.id;
+  const postData = await getPostData(id);
+  const msg = await sayHello();
+
+  return {
+    props: {
+      postData,
+    },
+  };
+}
